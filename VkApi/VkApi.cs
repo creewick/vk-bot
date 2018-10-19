b36d5c691e75b43c34c46f568fbb27bf6ad3c714
Юхатский Илья Владиславович <ilia.y@skbkontur.ru>
2018-10-19 16:02:30 +0500

    Vk API

---
+++ VkApi/VkApi.cs
@@ -0,0 +1,20 @@
+﻿using VkNet.Model;
+using VkNet.Model.RequestParams;
+
+namespace VkApi
+{
+    public class VkApi
+    {
+        private readonly VkNet.VkApi api = new VkNet.VkApi();
+
+        public void Login(string token)
+        {
+            api.Authorize(new ApiAuthParams{AccessToken = token});       
+        }
+
+        public void Send(long peerId, string message)
+        {
+            api.Messages.Send(new MessagesSendParams {Message = message, PeerId = peerId});
+        }
+    }
+}
---
+++ VkBot/Bot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using VKCallbackApi;
+
+namespace VkBot
+{
+    public class Bot
+    {
+        private readonly VkApi.VkApi api = new VkApi.VkApi();
+        private readonly VKCallbackApiHandler callback;
+
+        public Bot(string token)
+        {
+            api.Login(token);
+            callback = new VKCallbackApiHandler {ConfirmationToken = "f8ff207a"};
+            callback.OnRequest += req =>
+            {
+                if (req.Type == VKCallbackApi.Models.Enums.EventType.MessageNew)
+                {
+                    Send(req.Object.Id, "Да, привет!");
+                }
+            };
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine('a');
+            }
+        }
+
+        public void Send(long peerId, string message) => api.Send(peerId, message);
+    }
+}
